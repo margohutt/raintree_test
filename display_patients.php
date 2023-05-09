e27b0a5d70a869befe3b60beb75495bc0a11a0bb
margohu <marxhytt@gmail.com>
2023-05-09 22:41:08 +0300

    Exercise 2a and 2b

---
+++ display_patients.php
@@ -0,0 +1,39 @@
+<?php
+// Open the connection
+require_once 'mysql_connection.php';
+
+// Use the raintree database
+mysqli_select_db($connection, "raintree");
+
+// Query to retrieve all patients' information and insurance data ordered by insurance earliest date and patient last name
+$query = "SELECT patient.pn, patient.last, patient.first, insurance.iname, DATE_FORMAT(insurance.from_date, '%m-%d-%y') as from_date, DATE_FORMAT(insurance.to_date, '%m-%d-%y') as to_date
+FROM patient
+INNER JOIN insurance ON patient._id = insurance.patient_id
+ORDER BY insurance.from_date ASC, patient.last ASC";
+
+// Execute the query and store the result in $result
+$result = mysqli_query($connection, $query);
+
+// Check if the query was successful
+if ($result) {
+
+    // Loop over the result set and output each row in a separate line
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo $row['pn'] . ",";
+        echo $row['last'] . ",";
+        echo $row['first'] . ",";
+        echo $row['iname'] . ",";
+        echo $row['from_date'] . ",";
+        echo $row['to_date'] . "\n";
+    }
+
+    // Free up the memory used by the result
+    mysqli_free_result($result);
+} else {
+
+    // Display an error message if the query failed
+    echo "Error: " . mysqli_error($connection);
+}
+
+// Close the connection
+mysqli_close($connection);
---
+++ display_patients_stat.php
@@ -0,0 +1,44 @@
+<?php
+// Open the connection
+require_once 'mysql_connection.php';
+
+// Use the raintree database
+mysqli_select_db($connection, "raintree");
+
+// Retrieve full name from the database as uppercase letters
+$sql = "SELECT UPPER(first) AS firstname, UPPER(last) AS lastname FROM patient";
+
+//Store frequency of each character in the database
+$result = mysqli_query($connection, $sql);
+if (mysqli_num_rows($result) > 0) {
+    $character_counts = array();
+    $number_of_letters = 0;
+    while ($row = mysqli_fetch_assoc($result)) {
+        $name = $row["firstname"] . " " . $row["lastname"];
+        $number_of_letters += strlen($name) - substr_count($name, " ");
+        for ($i = 0; $i < strlen($name); $i++) {
+            $character = substr($name, $i, 1);
+            if ($character != " ") {
+                if (isset($character_counts[$character])) {
+                    $character_counts[$character]++;
+                } else {
+                    $character_counts[$character] = 1;
+                }
+            }
+        }
+    }
+
+// Sort the array by the count of each character
+    ksort($character_counts);
+
+// Output the results in a tabular format
+    foreach ($character_counts as $character => $count) {
+        $percent = ($count / $number_of_letters) * 100;
+        echo "$character\t$count\t" . number_format($percent, 2) . "%\n";
+    }
+
+// Display the total number of letters without spaces
+    echo "Total number of letters without spaces: $number_of_letters\n";
+
+} else { echo "No results found"; }
+mysqli_close($connection);
